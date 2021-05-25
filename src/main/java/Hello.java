080d8b1dca50a021c0e3b47794e28d76eb022210
Andreea Druga <andreea.druga51@gmail.com>
2021-05-25 15:33:40 -0500

    added more work

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
         System.out.println("Developing hello feature");
         System.out.println("Added more work");
         System.out.println("Local Repo is already published to GitHub");
+        System.out.println("Doing more work");
 
 
     }
